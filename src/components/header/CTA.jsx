0986c36eb1f63ba6d3692623e1eb73b4a3cf5695
parrishstefan <stefannparrish@gmail.com>
2023-04-05 18:32:42 -0700

    new resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/cv.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href="https://drive.google.com/file/d/1f7ZV0cOwUiGxMSiuKilW1sNPAFlV5Qul/view?usp=sharing" target="_blank" className='btn'>View CV</a>
+        <a href="https://drive.google.com/file/d/1aOiVDUNGxuN6vNFowPloL16I7T-WiJn8/view?usp=sharing" target="_blank" className='btn'>View CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
